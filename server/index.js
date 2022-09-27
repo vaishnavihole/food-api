03bd332d1d368b0277efbfa1db53587571da5e05
vaishnavihole <vaishnavihole1@gmail.com>
2022-09-27 09:57:02 +0530

    routes created

--- server/index.js
+++ server/index.js
@@ -1,17 +1,51 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Fooditem = require('./models/Fooditem');
 require('dotenv').config();
 
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, ()=> {
+mongoose.connect(process.env.MONGO_URI, () => {
     console.log('Connected To Mongodb');
 })
 
-app.listen(PORT, () =>{
+app.get('/health', (req, res) => {
+    res.json({
+        status: "All Good 👍",
+    });
+})
+
+app.post('/fooditem', async (req, res) => {
+
+    const { title, description, price, imageurl } = req.body
+
+    const newFooditem = new Fooditem({
+        title: title,
+        description: description,
+        price: price,
+        imageurl: imageurl
+    })
+
+    const savedFooditem = await newFooditem.save();
+
+    res.json({ savedFooditem })
+});
+
+app.get('/fooditem',async(req,res) => {
+
+    const Fooditems =await Fooditem.find();
+
+    res.json(Fooditems);
+    
+} )
+
+
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}📦`)
 })
--- server/models/Fooditem.js
+++ server/models/Fooditem.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
 const foodSchema = mongoose.Schema({
     title: {type:String, require:true},
     description: {type:String, require:true},
     price : {type:Number, require:true},
     imageurl : {type:String, require:true}
-});
+})
 
 module.exports = mongoose.model('Fooditem',foodSchema);
+
